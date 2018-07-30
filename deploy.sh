bc00d092199d67ba5a9a65b780d4d11843bb6301
Mihai Peteu <jericho1ne@yahoo.com>
2018-07-29 20:58:42 -0700

    minor

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-rsync -v -i -p dist/.htaccess mpeteu#uzabl.com@delta.hostineer.com:/var/www/html/starterkitmedia/
-rsync -v -i -p dist/index.html mpeteu#uzabl.com@delta.hostineer.com:/var/www/html/starterkitmedia/
+rsync -v -i -p dist/.htaccess mpeteu@uzabl.com@delta.hostineer.com:/var/www/html/starterkitmedia/
+rsync -v -i -p dist/index.html mpeteu@uzabl.com@delta.hostineer.com:/var/www/html/starterkitmedia/
 
 rsync -v -i -p -r --exclude="*.liquid" --exclude="*.psd" --exclude="*.xml" --exclude="*.mp4" --exclude="*.webm" \
-  dist/ mpeteu#uzabl.com@delta.hostineer.com:/var/www/html/starterkitmedia
+  dist/ mpeteu@uzabl.com@delta.hostineer.com:/var/www/html/starterkitmedia
